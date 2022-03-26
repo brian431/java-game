4b0d06a0af52a8186f5b61ccc2bff9eb3455a79d
brian <botinholmes@gmail.com>
2022-03-26 15:13:01 +0800

    圖片用不了幹明明STANDING的url是沒問題的

--- src/main/java/scenes/Level1.java
+++ src/main/java/scenes/Level1.java
@@ -1,16 +1,33 @@
 package scenes;
 
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-
+import javafx.scene.shape.Rectangle;
+import model.Player;
 public class Level1 extends Level {
     final int WIDTH = 1366;
     final int Height = 768;
     public Scene scene;
-    public Pane pane;
+    public Pane rootPane;
+
+    Player player;
+    Rectangle mainFloor;
+
+
 
     public Level1() {
-        pane = new Pane();
-        scene = new Scene(pane, WIDTH, Height);
+        rootPane = new Pane();
+        scene = new Scene(rootPane, WIDTH, Height);
+
+        player = new Player();
+        setScene();
+    }
+
+    void setScene() {
+        mainFloor = new Rectangle(0, 700, 1367, 68);
+        rootPane.getChildren().add(mainFloor);
+        rootPane.getChildren().add(player.hitbox);
     }
 }
