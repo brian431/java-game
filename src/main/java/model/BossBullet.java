5ed0d652db5f6bb5d65c0ee913eb1189d15e054d
boson13579 <boson13579@gmail.com>
2022-03-30 23:06:32 +0800

    renew projectile

---
+++ src/main/java/model/BossBullet.java
@@ -0,0 +1,10 @@
+package model;
+
+import javafx.geometry.Point2D;
+
+public class BossBullet extends Projectile{
+
+    public BossBullet(String type, double X, double Y, Point2D vector) {
+        super(type, X, Y, vector);
+    }
+}
---
+++ src/main/java/model/PlayerBullet.java
@@ -0,0 +1,10 @@
+package model;
+
+import javafx.geometry.Point2D;
+
+public class PlayerBullet extends Projectile{
+
+    public PlayerBullet(String type, double X, double Y, Point2D vector) {
+        super(type, X, Y, vector);
+    }
+}
